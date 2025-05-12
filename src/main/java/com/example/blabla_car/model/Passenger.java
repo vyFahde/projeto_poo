5b93af3adfe10374215cd8e0de142bc83a12ffd4
vyFahde <txneto2005@gmail.com>
2025-05-12 20:12:22 -0300

    Criação de classes

---
+++ src/main/java/com/example/blabla_car/model/Passenger.java
@@ -0,0 +1,16 @@
+package com.example.blabla_car.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
