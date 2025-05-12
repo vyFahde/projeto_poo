9d6ec93aa8f9cbe0831e509a992f22629047ba99
vyFahde <txneto2005@gmail.com>
2025-05-12 14:36:04 -0300

    Meu commit

---
+++ src/main/java/com/example/blabla_car/BlablaCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.blabla_car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablaCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlablaCarApplication.class, args);
+	}
+
+}
